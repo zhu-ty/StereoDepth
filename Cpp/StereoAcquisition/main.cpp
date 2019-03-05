2c74e2b12cee0f87fd0b7456fd0a468905a1ef2a
zhu-ty <zhu-ty@outlook.com>
2019-03-05 20:12:01 +0800

    re-write StereoAcquisition project, force hardware sync

--- Cpp/StereoAcquisition/main.cpp
+++ Cpp/StereoAcquisition/main.cpp
@@ -5,7 +5,7 @@ Online Stereo Image Capture
 @date Apr 24, 2018
 */
 
-#include "StereoAcquisition.h"
+// #include "StereoAcquisition.h"
 
 #include "NPPJpegCoder.h"
 #include "GenCameraDriver.h"
@@ -14,24 +14,46 @@ Online Stereo Image Capture
 
 //TODO 这个项目本来是实时采集和标定的，现在只剩下采集的功能，因为实时标定太慢了，故将两个流程分开
 
+cv::cuda::GpuMat applyWhiteBalanceBGR(cv::cuda::GpuMat img,
+	float red, float green, float blue) {
+	Npp32f wbTwist[3][4] = {
+		{ 1.0, 0.0, 0.0, 0.0 },
+		{ 0.0, 1.0, 0.0, 0.0 },
+		{ 0.0, 0.0, 1.0, 0.0 }
+	};
+	// update wbTwist
+	wbTwist[0][0] = blue;
+	wbTwist[1][1] = green;
+	wbTwist[2][2] = red;
+	NppiSize osize;
+	osize.width = img.cols;
+	osize.height = img.rows;
+	nppiColorTwist32f_8u_C3IR(img.data,
+		img.step, osize, wbTwist);
+	return img;
+}
+
+
 int main(int argc, char* argv[]) {
 	std::vector<cam::GenCamInfo> camInfos;
-	cam::CameraModel cm = cam::CameraModel::XIMEA_xiC;
-	std::shared_ptr<cam::GenCamera> cameraPtr = cam::createCamera(cm);
+	std::shared_ptr<cam::GenCamera> cameraPtr = cam::createCamera(cam::CameraModel::XIMEA_xiC);
 	//std::shared_ptr<cam::GenCamera> cameraPtr = cam::createCamera(cam::CameraModel::PointGrey_u3);
 	cameraPtr->init();
+
+	cameraPtr->setSyncType(cam::GenCamSyncType::Hardware); //HARDWARE NEEDED!
+
 	// set camera setting
 	cameraPtr->setFPS(-1, 10);
-	cameraPtr->setCamBufferType(cam::GenCamBufferType::Raw);
 	cameraPtr->startCapture();
 	
 	//cameraPtr->setAutoExposure(-1, cam::Status::on);
 	//cameraPtr->setAutoExposureLevel(-1, 20);
-	cameraPtr->setExposure(-1, 10000);
-	cameraPtr->setWhiteBalance(-1, 2.0, 1.0, 2.0);
+	cameraPtr->setExposure(-1, 20000);
+	//cameraPtr->setWhiteBalance(-1, 2.0, 1.0, 2.0); 
 	cameraPtr->makeSetEffective();
 	// set capturing setting
 	
+	cameraPtr->setCamBufferType(cam::GenCamBufferType::Raw);
 	cameraPtr->setCaptureMode(cam::GenCamCaptureMode::Continous, 200);
 	cameraPtr->setCapturePurpose(cam::GenCamCapturePurpose::Streaming);
 	//cameraPtr->setAutoWhiteBalance(-1);
@@ -39,31 +61,12 @@ int main(int argc, char* argv[]) {
 	cameraPtr->getCamInfos(camInfos);
 	cameraPtr->startCaptureThreads();
 
-	cv::Mat K1 = cv::Mat::zeros(3, 3, CV_32F);
-	K1.at<float>(0, 0) = 7138;
-	K1.at<float>(1, 1) = 7138;
-	K1.at<float>(2, 2) = 1;
-	K1.at<float>(0, 2) = 1232;
-	K1.at<float>(1, 2) = 1028;
-	cv::Mat K2 = K1.clone();
-	cv::Size chessBoardSize(11, 8);
-
-	//cv::Mat img1 = cv::imread("1.png");
-	//cv::Mat img2 = cv::imread("2.png");
-
-	StereoCalibration stereoCalibrator;
-	stereoCalibrator.init(K1, K2, chessBoardSize);
 
 	std::vector<cam::Imagedata> imgdatas(2);
 	std::vector<cv::Mat> imgs(2);
 	std::vector<cv::Mat> imgs_c(2);
 	std::vector<cv::cuda::GpuMat> imgs_bayer_d(2);
 	std::vector<cv::cuda::GpuMat> imgs_d(2);
-
-	//cv::Mat wbMat(camInfos[0].height, camInfos[0].width, CV_8UC3);
-	//wbMat.setTo(cv::Scalar(2, 1, 2));
-	//cv::cuda::GpuMat wbMat_d;
-	//wbMat_d.upload(wbMat);
 
 	SysUtil::sleep(1000);
 	int nframe = 0;
@@ -72,39 +75,45 @@ int main(int argc, char* argv[]) {
 	SysUtil::mkdir("./images");
 	SysUtil::mkdir("./images/0");
 	SysUtil::mkdir("./images/1");
+	cv::Mat display;
 	for (;;) {
-		std::cout << nframe++ << std::endl;
+
 		cameraPtr->captureFrame(imgdatas);
 
-		imgs[0] = cv::Mat(camInfos[0].height, camInfos[0].width,
-			CV_8U, reinterpret_cast<void*>(imgdatas[0].data));
-		imgs[1] = cv::Mat(camInfos[1].height, camInfos[1].width,
-			CV_8U, reinterpret_cast<void*>(imgdatas[1].data));
-
-		imgs_bayer_d[0].upload(imgs[0]);
-		imgs_bayer_d[1].upload(imgs[1]);
-
-		//cv::Mat tmp;
-		//cv::demosaicing(imgs[0], tmp, npp::bayerPatternNPP2CVRGB(static_cast<NppiBayerGridPosition>(
-		//	static_cast<int>(camInfos[0].bayerPattern))), -1);
-
-		cv::cuda::demosaicing(imgs_bayer_d[0], imgs_d[0], 
-			npp::bayerPatternNPP2CVRGB(static_cast<NppiBayerGridPosition>(
-			static_cast<int>(camInfos[0].bayerPattern))), -1);
-		cv::cuda::demosaicing(imgs_bayer_d[1], imgs_d[1], 
-			npp::bayerPatternNPP2CVRGB(static_cast<NppiBayerGridPosition>(
-			static_cast<int>(camInfos[1].bayerPattern))), -1);
-		if (cm == cam::CameraModel::XIMEA_xiC)
+		for (int i = 0; i < 2; i++)
 		{
-			imgs_d[0] = StereoCalibration::applyWhiteBalance(imgs_d[0], 2.0, 1, 2.0);
-			imgs_d[1] = StereoCalibration::applyWhiteBalance(imgs_d[1], 2.0, 1, 2.0);
+			imgs[i] = cv::Mat(camInfos[i].height, camInfos[i].width,
+				CV_8U, reinterpret_cast<void*>(imgdatas[i].data));
+			imgs_bayer_d[i].upload(imgs[i]);
+			cv::cuda::demosaicing(imgs_bayer_d[i], imgs_d[i],
+				npp::bayerPatternNPP2CVRGB(static_cast<NppiBayerGridPosition>(
+					static_cast<int>(camInfos[i].bayerPattern))), -1);
+			cv::cuda::cvtColor(imgs_d[i], imgs_d[i], cv::COLOR_RGB2BGR);
+			if (camInfos[i].isWBRaw == false)
+			{
+				imgs_d[i] = applyWhiteBalanceBGR(imgs_d[i], 1.89, 1, 1.45); //RGB
+			}
+			imgs_d[i].download(imgs_c[i]);
 		}
 
-		imgs_d[0].download(imgs_c[0]);
-		imgs_d[1].download(imgs_c[1]);
+		cv::Size smallSize(imgs_c[0].cols / 4, imgs_c[0].rows / 4);
+		cv::Mat smallImg1, smallImg2;
+		cv::resize(imgs_c[0], smallImg1, smallSize);
+		cv::resize(imgs_c[1], smallImg2, smallSize);
+		display.create(smallImg1.rows, smallImg1.cols * 2, CV_8UC3);
 
-		stereoCalibrator.SaveImg(imgs_c[0], imgs_c[1]);
+		cv::Rect rect(0, 0, smallImg1.cols, smallImg1.rows);
+		smallImg1.copyTo(display(rect));
+		rect.x += smallImg1.cols;
+		smallImg2.copyTo(display(rect));
+		cv::imshow("corners", display);
+
+		cv::imwrite(cv::format("images/0/%05d.png", nframe), imgs_c[0]);
+		cv::imwrite(cv::format("images/1/%05d.png", nframe), imgs_c[1]);
+		cv::waitKey(1);
 		SysUtil::sleep(50);
+
+		std::cout << nframe++ << std::endl;
 	}
 
 	cameraPtr->stopCaptureThreads();
